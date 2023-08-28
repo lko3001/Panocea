288512474c17fa67e0ccf0b981bb085131aabb9b
Lko <3001lorenzomaino@gmail.com>
2023-08-28 19:36:56 +0200

    Removed PWA

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
-
-const nextConfig = withPWA({
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  outputFileTracing: false,
-});
-module.exports =
-  process.env.NODE_ENV === "development" ? nextConfig : nextConfig;
+};
+module.exports = nextConfig;
